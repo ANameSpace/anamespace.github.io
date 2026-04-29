dfb87b4db63e77bb9dee51246fad39a53de2aae7
ANameSpace <125720576+ANameSpace@users.noreply.github.com>
2026-04-29 10:23:52 +0300

    add simpleanalytics

--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -1,9 +1,9 @@
 <script>
-  import { page } from '$app/state';
+	import { page } from '$app/state';
 </script>
 
 <main style="text-align: center; padding: 4rem;">
-  <h1>404</h1>
-  <p>Страница <strong>{page.url.pathname}</strong> не найдена</p>
-  <a href="/">← Вернуться на главную</a>
+	<h1>404</h1>
+	<p>Страница <strong>{page.url.pathname}</strong> не найдена</p>
+	<a href="/">← Вернуться на главную</a>
 </main>
